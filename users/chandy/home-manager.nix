bd943eb08e19b3446a2434b5a0d0b4f5859e256e
Chris Handy <chrisjhandy@gmail.com>
2025-12-28 17:16:52 -0600

    removing git config

--- users/chandy/home-manager.nix
+++ users/chandy/home-manager.nix
@@ -25,12 +25,17 @@ let
     js = "jj st";
 
     wo = "cd ~/Developer/workspace";
+
+    vi = "nvim";
+    vim = "nvim";
   } // (if isLinux then {
     # Two decades of using a Mac has made this such a strong memory
     # that I'm just going to keep it consistent.
     pbcopy = "xclip";
     pbpaste = "xclip -o";
-  } else {});
+  } else { 
+    blah = "echo blah";
+  });
 
   # For our MANPAGER env var
   # https://github.com/sharkdp/bat/issues/1145
@@ -198,63 +203,8 @@ in {
     # ];
   };
 
-  programs.git = {
-    enable = true;
-    userName = "Chris Handy";
-    userEmail = "chandy@peoplescout.com";
-    aliases = {
-      cleanup = "!git branch --merged | grep  -v '\\*\\|master\\|develop' | xargs -n 1 -r git branch -d";
-      prettylog = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
-      root = "rev-parse --show-toplevel";
-      # View abbreviated SHA, description, and history graph of the latest 20 commits
-      l = "log --pretty=oneline -n 20 --graph --abbrev-commit";
-      # View the current working tree status using the short formats
-      s = "status -s";
-      # Show the diff between the latest commit and the current state
-      d = "!git diff-index --quiet HEAD -- || clear; git --no-pager diff --patch-with-stat";
-      # `git di $number` shows the diff between the state `$number` revisions ago and the current state
-      di = "!d() { git diff --patch-with-stat HEAD~$1; }; git diff-index --quiet HEAD -- || clear; d";
-      # Using diff-so-fancy
-      dsf = "!git diff --color $@ | diff-so-fancy";
-      # Pull in remote changes for the current repository and all its submodules
-      p = "!git pull; git submodule foreach git pull origin master";
-      # Clone a repository including all submodules
-      c = "clone --recursive";
-      # Commit all changes
-      ca = "!git add -A && git commit -av";
-      # Switch to a branch, creating it if necessary
-      go = "!f() { git checkout -b \"$1\" 2> /dev/null || git checkout \"$1\"; }; f";
-      # Show verbose output about tags, branches or remote
-      tags = "tag -l";
-      branches = "branch -a";
-      remotes = "remote -v";
-      # Amend the currently staged files to the latest commit
-      amend = "commit --amend --reuse-message=HEAD";
-      # Credit an author on the latest commit
-      credit = "!f() { git commit --amend --author \"$1 <$2>\" -C HEAD; }; f";
-      # Interactive rebase with the given number of latest commits
-      reb = "!r() { git rebase -i HEAD~$1; }; r";
-      # Remove the old tag with this name and tag the latest commit with it.
-      retag = "!r() { git tag -d $1 && git push origin :refs/tags/$1 && git tag $1; }; r";
-      # Find branches containing commit
-      fb = "!f() { git branch -a --contains $1; }; f";
-      # Find tags containing commit
-      ft = "!f() { git describe --always --contains $1; }; f";
-      # Find commits by source code
-      fc = "!f() { git log --pretty=format:'%C(yellow)%h  %Cblue%ad  %Creset%s%Cgreen  [%cn] %Cred%d' --decorate --date=short -S$1; }; f";
-      # Find commits by commit message
-      fm = "!f() { git log --pretty=format:'%C(yellow)%h  %Cblue%ad  %Creset%s%Cgreen  [%cn] %Cred%d' --decorate --date=short --grep=$1; }; f";
-    };
-    extraConfig = {
-      branch.autosetuprebase = "always";
-      color.ui = true;
-      core.askPass = ""; # needs to be empty to use terminal for ask pass
-      credential.helper = "store"; # want to make this more secure
-      github.user = "chandy";
-      push.default = "tracking";
-      init.defaultBranch = "main";
-    };
-  };
+  # Git configuration now managed by chezmoi
+  # See: ~/.config/git/config
 
   programs.go = {
     enable = true;
@@ -267,23 +217,6 @@ in {
 
     # I don't use "settings" because the path is wrong on macOS at
     # the time of writing this.
-  };
-
-  programs.alacritty = {
-    enable = !isWSL;
-
-    settings = {
-      env.TERM = "xterm-256color";
-
-      key_bindings = [
-        { key = "K"; mods = "Command"; chars = "ClearHistory"; }
-        { key = "V"; mods = "Command"; action = "Paste"; }
-        { key = "C"; mods = "Command"; action = "Copy"; }
-        { key = "Key0"; mods = "Command"; action = "ResetFontSize"; }
-        { key = "Equals"; mods = "Command"; action = "IncreaseFontSize"; }
-        { key = "Subtract"; mods = "Command"; action = "DecreaseFontSize"; }
-      ];
-    };
   };
 
   programs.i3status = {
@@ -306,68 +239,10 @@ in {
   programs.neovim = {
     enable = true;
     # package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
-
-    withPython3 = true;
-    viAlias = true;
-    vimAlias = true;
-
-    # plugins = with pkgs; [
-
-    #   vimPlugins.which-key-nvim
-    #   vimPlugins.plenary-nvim
-    #   vimPlugins.popup-nvim
-    #   vimPlugins.telescope-nvim
-    #   vimPlugins.telescope-fzf-native-nvim
-    #   vimPlugins.telescope-file-browser-nvim
-    #   vimPlugins.nvim-treesitter
-    #   vimPlugins.nvim-treesitter-textobjects
-    #   vimPlugins.nvim-treesitter-refactor
-    #   vimPlugins.nvim-treesitter-context
-    #   vimPlugins.playground
-    #   vimPlugins.nvim-lspconfig
-    #   vimPlugins.lsp-status-nvim
-    #   vimPlugins.none-ls-nvim
-    #   vimPlugins.trouble-nvim
-    #   vimPlugins.nvim-cmp
-    #   vimPlugins.cmp-nvim-lsp
-    #   vimPlugins.cmp-buffer
-    #   vimPlugins.cmp-path
-    #   vimPlugins.cmp-cmdline
-    #   vimPlugins.cmp-treesitter
-    #   vimPlugins.cmp-spell
-    #   vimPlugins.cmp_luasnip
-    #   vimPlugins.luasnip
-    #   vimPlugins.lualine-nvim
-    #   vimPlugins.kanagawa-nvim
-    #   vimPlugins.nvim-web-devicons
-    #   vimPlugins.vim-startify
-    #   vimPlugins.neoformat
-    # ];
-
-    # # extraConfig = (import ./vim-config.nix) { inherit sources; };
-    # extraConfig = ''
-    #   lua require('base')
-    # '';
-
-    # extraPackages = with pkgs; [
-    #   # Language server packages (executables)
-    #   lua-language-server
-    #   nodePackages.vim-language-server
-    #   stylua
-    #   nixfmt-rfc-style
-    #   rustfmt
-    # ];    
   };
 
   programs.atuin = {
     enable = true;
-    enableFishIntegration = true;
-    enableNushellIntegration = true;
-    settings = {
-      show_tabs = false;
-      style = "compact";
-      enter_accept = true;
-    };
   };
 
   programs.nushell = {
@@ -413,23 +288,6 @@ in {
 
   programs.starship = {
     enable = true;
-  };
-
-  # # Htop
-  # # https://rycee.gitlab.io/home-manager/options.html#opt-programs.htop.enable
-  # # programs.htop.enable = true;
-  # # programs.htop.settings.show_program_path = true;
-
-  programs.starship.settings = {
-    # See docs here: https://starship.rs/config/
-    # Symbols config configured ./starship-symbols.nix.
-
-    directory.fish_style_pwd_dir_length = 1; # turn on fish directory truncation
-    directory.truncation_length = 2; # number of directories not to truncate
-    gcloud.disabled = true; # annoying to always have on
-    hostname.style = "bold green"; # don't like the default
-    memory_usage.disabled = true; # because it includes cached memory it's reported as full a lot
-    username.style_user = "bold blue"; # don't like the default
   };
 
   programs.zellij = { enable = true; };
